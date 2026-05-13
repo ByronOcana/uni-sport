488d9c53cdb35cbdd0399ff969b5443aa3be133c
iNothingAtAll <inaa.from_light@proton.me>
2026-05-13 17:33:18 -0500

    [add] se agregaron logs para el servicio de pagos

--- tallerparejas/gateway/app.py
+++ tallerparejas/gateway/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
+from time import time
 import requests
+
 
 app = Flask(__name__)
 
@@ -15,8 +17,24 @@ def inventario():
 
 @app.route('/pagos')
 def pagos():
-    response = requests.get('http://pagos:5002/pagos')
-    return jsonify(response.json())
+    try:
+        tiempo_inicio = time()
+        print("[PAGOS]: Se inicio la peticion de servicio interno de pagos", flush=True)
+        response = requests.get('http://pagos:5002/pagos', timeout=2)    
+        tiempo_final = time()
+        print("[PAGOS]: El servicio respondio a la peticion", flush=True)
+        print(f"[PAGOS] El servicio se ha demorando {tiempo_final - tiempo_inicio}seg", flush=True)
+        return jsonify(response.json())
+
+    except requests.exceptions.Timeout:
+        print("[PAGOS: ERROR]: El servicio demora mucho en responder", flush=True)
+        return jsonify({"error": "El servicio esta demorando mucho"})
+
+    except requests.exceptions.ConnectionError:
+        print("[PAGOS: ERROR]: No se a podido hacer una conexcion con el serivicio", flush=True)
+        return jsonify({"error": "El no esta disponible"})
+
+
 
 @app.route('/monitor')
 def monitor():
