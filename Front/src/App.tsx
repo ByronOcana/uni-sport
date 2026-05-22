a4320fb436f4912ca78a49fcf9a27a6744e288f9
Andres Otero <robinsonotero70@gmail.com>
2026-05-22 14:24:58 -0500

    agregue rutas (bets, history, dashboard

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginPage from "./Pages/LoginPage"
 import Dashboardpage from "./Pages/DashboardPage"
 import RegisterPage from "./Pages/RegisterPage"
+import BetsPage from "./Pages/BetsPage"
+import BetHistoryPage from "./Pages/BetHistoryPage"
 import "./App.css"
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/profile" element={<Dashboardpage />} />
         <Route path="/Registro" element={<RegisterPage />} />
+        <Route path="/bets" element={<BetsPage />} />
+        <Route path="/history" element={<BetHistoryPage />} />
       </Routes>
     </BrowserRouter>
   )
-
 }
 
 export default App
